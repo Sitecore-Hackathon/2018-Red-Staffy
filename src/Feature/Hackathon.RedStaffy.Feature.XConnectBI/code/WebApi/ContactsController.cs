9e7e9007bcd02136713b4e14947000f1ce72eba4
SWITCHITLOCAL\zheny <zhen.yuan@switchit.com>
2018-03-03 13:06:17 +1100

    Update project

---
+++ src/Feature/Hackathon.RedStaffy.Feature.XConnectBI/code/WebApi/ContactsController.cs
@@ -0,0 +1,10 @@
+﻿using Sitecore.Services.Core;
+using Sitecore.Services.Infrastructure.Web.Http;
+
+namespace Hackathon.RedStaffy.XConnectBI.WebApi
+{
+    [ServicesController]
+	public class ContactsController: ServicesApiController
+    {
+	}
+}
